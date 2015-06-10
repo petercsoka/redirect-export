5051e48b7871132ddf6e8270432563bfc4c501ca
petercsoka <petercsoka@gmail.com>
2015-06-10 16:49:56 +0100

    Add file path to write function

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var fs = require("fs")
     , csv = require("./formats/csv")
     , htaccess = require("./formats/htaccess")
-;
+    ;
 
-function Redirect(logging_file)
+function Redirect()
 {
-    if(this instanceof Redirect == false) return new Redirect(logging_file);
-
-    this.stream = fs.createWriteStream(logging_file);
+    if(this instanceof Redirect == false) return new Redirect();
 
     this.list = [];
 }
@@ -17,11 +15,13 @@ Redirect.prototype.add = function addFN(old_url, new_url)
     this.list.push([old_url,new_url]);
 };
 
-Redirect.prototype.write = function writeFn(format)
+Redirect.prototype.write = function writeFn(format, file)
 {
+    var stream = fs.createWriteStream(file);
+
     switch(format) {
-        case "csv"      : this.stream.write(csv(this.list));        break;
-        case "htaccess" : this.stream.write(htaccess(this.list));   break;
+        case "csv"      : stream.write(csv(this.list));        break;
+        case "htaccess" : stream.write(htaccess(this.list));   break;
     }
 };
 
