0dc687e7f3fdb18ad28003b9214a63b20bb0d7f4
petercsoka <petercsoka@gmail.com>
2015-06-12 14:36:41 +0100

    Add appendFile to add() funcion

--- index.js
+++ index.js
@@ -3,26 +3,44 @@ var fs = require("fs")
     , htaccess = require("./formats/htaccess")
     ;
 
-function Redirect()
+function Redirect(config)
 {
-    if(this instanceof Redirect == false) return new Redirect();
+    if(this instanceof Redirect == false) return new Redirect(config);
 
     this.list = [];
+    this.config = config || {};
+    this.config.format = config.format || "csv";
+    this.config.file = config.file || "redirects.csv";
 }
+
+Redirect.prototype.formatFactory = function formatFactoryFN(format, data)
+{
+    switch(format) {
+        case "csv"      : return csv(data);        break;
+        case "htaccess" : return htaccess(data);   break;
+        default         : throw Error("No script specified for "+ format + " format"); break;
+    }
+};
 
 Redirect.prototype.add = function addFN(old_url, new_url)
 {
-    this.list.push([old_url,new_url]);
+    var entry = [ old_url, new_url ];
+    this.list.push(entry);
+    var format_output = this.formatFactory(this.config.format, [entry]);
+
+    if (typeof this.config.inMemory == "undefined" || this.config.inMemory == false) {
+        fs.appendFile(this.config.file, format_output + "\n", function (err) {
+            if (err) throw err;
+        });
+    }
 };
 
 Redirect.prototype.write = function writeFn(format, file)
 {
     var stream = fs.createWriteStream(file);
 
-    switch(format) {
-        case "csv"      : stream.write(csv(this.list));        break;
-        case "htaccess" : stream.write(htaccess(this.list));   break;
-    }
+    stream.write(this.formatFactory(format, this.list));
+
 };
 
 Redirect.prototype.close = function closeFN()
